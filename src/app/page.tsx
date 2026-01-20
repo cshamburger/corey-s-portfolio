cd2d6160c8a64339772b1d3949a2db1903d416b4
CTSham <https://github.com/CTSham>
2026-01-20 15:50:06 -0800

    Updated src/app/page.tsx and layout.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Hero from '@/components/Home/Hero';
+import Counter from '@/components/Home/Counter'
+import Progresswork from '@/components/Home/WorkProgress';
+import Services from '@/components/Home/Services';
+import Portfolio from '@/components/SharedComponent/portfollio'
+import Testimonial from '@/components/SharedComponent/Testimonial'
+import Blog from '@/components/SharedComponent/Blog'
+import Contactform from '@/components/Home/Contact';
+
+
+export default function Home() {
+    return (
+        <main>
+            <Hero />
+            <Counter isColorMode={false} />
+            <Progresswork isColorMode={false} />
+            <Services />
+            <Portfolio />
+            <Testimonial />
+            <Blog />
+            <Contactform />
+        </main>
+    )
+}
